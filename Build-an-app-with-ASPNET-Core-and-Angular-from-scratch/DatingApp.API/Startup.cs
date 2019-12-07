9f7f4fca6ce0a8b11e61cd6d8c009dddfe109677
SergiOn <onishhenko@gmail.com>
2019-12-07 20:00:40 +0200

    #build-an-app-with-aspnet-core-and-angular-from-scratch # section 2, lecture 7 # updated code styling

--- Build-an-app-with-ASPNET-Core-and-Angular-from-scratch/DatingApp.API/Startup.cs
+++ Build-an-app-with-ASPNET-Core-and-Angular-from-scratch/DatingApp.API/Startup.cs
@@ -11,28 +11,24 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace DatingApp.API
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace DatingApp.API {
+
+    public class Startup {
+
+        public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
 
@@ -42,8 +38,7 @@ namespace DatingApp.API
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
+            app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
             });
         }
--- Build-an-app-with-ASPNET-Core-and-Angular-from-scratch/DatingApp.API/WeatherForecast.cs
+++ Build-an-app-with-ASPNET-Core-and-Angular-from-scratch/DatingApp.API/WeatherForecast.cs
@@ -1,9 +1,9 @@
 using System;
 
-namespace DatingApp.API
-{
-    public class WeatherForecast
-    {
+namespace DatingApp.API {
+
+    public class WeatherForecast {
+
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
