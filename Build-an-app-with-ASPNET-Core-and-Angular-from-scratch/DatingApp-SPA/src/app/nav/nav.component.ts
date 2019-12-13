7981644d12f14f09812d950e12c98e59e7df3bec
SergiOn <onishhenko@gmail.com>
2019-12-13 15:49:18 +0200

    #build-an-app-with-aspnet-core-and-angular-from-scratch # section 4, lecture 42 # Introduction to Angular Services

--- Build-an-app-with-ASPNET-Core-and-Angular-from-scratch/DatingApp-SPA/src/app/nav/nav.component.ts
+++ Build-an-app-with-ASPNET-Core-and-Angular-from-scratch/DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,20 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
+export class NavComponent {
 
   model: any = {
     username: '',
     password: ''
   };
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit() {
+  login() {
+    this.authService.login(this.model).subscribe(
+      next => {
+        console.log('Logged in successfully');
+      },
+      error => {
+        console.log('Failed to login');
+      }
+    );
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
   }
 
 }
