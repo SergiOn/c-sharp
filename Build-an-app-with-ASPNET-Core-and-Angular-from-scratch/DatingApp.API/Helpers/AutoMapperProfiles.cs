5e8e4664004c8421aacf5a1e9563991af540c692
SergiOn <onishhenko@gmail.com>
2019-12-20 22:55:25 +0200

    #build-an-app-with-aspnet-core-and-angular-from-scratch # section 10, lecture 101 # Persisting the Member updates

--- Build-an-app-with-ASPNET-Core-and-Angular-from-scratch/DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ Build-an-app-with-ASPNET-Core-and-Angular-from-scratch/DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -30,6 +30,7 @@ namespace DatingApp.API.Helpers {
                         opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
 
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
 
         }
 
