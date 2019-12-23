1de84f07016f98306f52e0bb81eddd95257803ab
SergiOn <onishhenko@gmail.com>
2019-12-23 12:22:51 +0200

    #build-an-app-with-aspnet-core-and-angular-from-scratch # section 11, lecture 113 # Adding the Set Main photo functionality to the API

--- Build-an-app-with-ASPNET-Core-and-Angular-from-scratch/DatingApp.API/Data/DatingRepository.cs
+++ Build-an-app-with-ASPNET-Core-and-Angular-from-scratch/DatingApp.API/Data/DatingRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
@@ -36,6 +37,11 @@ namespace DatingApp.API.Data {
         public async Task<Photo> GetPhoto(int id) {
             var photo = await context.Photos.FirstOrDefaultAsync(p => p.Id == id);
             return photo;
+        }
+
+        public async Task<Photo> GetMainPhotoForUser(int userId) {
+            return await context.Photos.Where(u => u.UserId == userId)
+                .FirstOrDefaultAsync(p => p.IsMain);
         }
 
         public async Task<bool> SaveAll() {
