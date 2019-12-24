b3e97718c492d39bf08301df772a50da6b8b2d62
SergiOn <onishhenko@gmail.com>
2019-12-24 13:45:21 +0200

    #build-an-app-with-aspnet-core-and-angular-from-scratch # section 12, lecture 124 # Introduction to Reactive Forms in Angular

--- Build-an-app-with-ASPNET-Core-and-Angular-from-scratch/DatingApp-SPA/src/app/register/register.component.ts
+++ Build-an-app-with-ASPNET-Core-and-Angular-from-scratch/DatingApp-SPA/src/app/register/register.component.ts
@@ -1,29 +1,40 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
   @Output() cancelRegister = new EventEmitter<void>();
 
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private authService: AuthService, private alertify: AlertifyService) {}
 
+  ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl(),
+    });
+  }
+
   register() {
-    this.authService.register(this.model).subscribe(
-      () => {
-        this.alertify.success('Registration successful');
-      },
-      error => {
-        this.alertify.error(error);
-      }
-    );
+    // this.authService.register(this.model).subscribe(
+    //   () => {
+    //     this.alertify.success('Registration successful');
+    //   },
+    //   error => {
+    //     this.alertify.error(error);
+    //   }
+    // );
+    console.log(this.registerForm);
   }
 
   cancel() {
