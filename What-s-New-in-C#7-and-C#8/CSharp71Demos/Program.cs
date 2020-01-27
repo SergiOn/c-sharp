b1107b333229521eb636a1f584918b7896d3a62d
SergiOn <onishhenko@gmail.com>
2020-01-27 22:38:22 +0200

    #csharp7-whats-new # section 2: What's New in C# 7.1 # created new project

---
+++ What-s-New-in-C#7-and-C#8/CSharp71Demos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharp71Demos {
+
+    class Program {
+
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+
+}
