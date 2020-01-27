7d15180b5070e02093cd799ba9d8ff181f14c151
SergiOn <onishhenko@gmail.com>
2020-01-27 22:44:33 +0200

    #csharp7-whats-new # section 2: What's New in C# 7.1 # lecture 13: Async Main

---
+++ What-s-New-in-C#7-and-C#8/CSharp71Demos/AsyncMain.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CSharp71Demos {
+
+    internal class AsyncMain {
+
+        // used to be the case that your demo
+        // would have to reside in a separate
+        // body
+
+        private static string url = "http://google.com/robots.txt";
+
+        //private static async Task MainAsync(string s)
+        //{
+        //  // blah
+        //  Console.WriteLine(await new HttpClient().GetStringAsync(s));
+        //}
+
+        //public static void Main(string[] args)
+        //{
+        //  // fine
+        //  MainAsync(url).GetAwaiter().GetResult();
+        //}
+
+    
+        // there is no async void, it's
+
+        // Task Main
+        // Task<int> Main if you need to return
+        static async Task Main(string[] args) {
+            Console.WriteLine(await new HttpClient().GetStringAsync(url));
+        }
+
+    }
+
+}
