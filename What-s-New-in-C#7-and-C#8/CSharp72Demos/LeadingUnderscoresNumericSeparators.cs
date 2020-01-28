a524dff9e9e86ca79e993713e8ecf41e44ae8000
SergiOn <onishhenko@gmail.com>
2020-01-28 10:11:26 +0200

    #csharp7-whats-new # section 3: What's New in C# 7.2 # lecture 18: Leading Digit Separators

---
+++ What-s-New-in-C#7-and-C#8/CSharp72Demos/LeadingUnderscoresNumericSeparators.cs
@@ -0,0 +1,15 @@
+namespace CSharp72Demos {
+
+    public class LeadingUnderscoresNumericSeparators {
+
+        public static void MainLUNS(string[] args) {
+            // binary
+            var x = 0b_1111_0000;
+
+            // hex
+            var y = 0x_baad_f00d;
+        }
+
+    }
+
+}
---
+++ What-s-New-in-C#7-and-C#8/CSharp72Demos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharp72Demos {
+
+    class Program {
+
+        static void Main(string[] args) {
+            // Console.WriteLine("Hello World!");
+            
+            LeadingUnderscoresNumericSeparators.MainLUNS(args);
+
+        }
+
+    }
+
+}
